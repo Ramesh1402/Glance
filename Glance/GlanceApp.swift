ce262d064c65264fda0c0ba19defa1b967c6e610
Ramesh1402 <reachrameshmobiledev@gmail.com>
2025-02-08 12:38:43 +0530

    Initial Commit

---
+++ Glance/GlanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlanceApp.swift
+//  Glance
+//
+//  Created by Ramesh Shanmugam on 08/02/25.
+//
+
+import SwiftUI
+
+@main
+struct GlanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
